6f46a4b260ec66678f5323ea3b5b35bc64f0db41
Arpit Singh <arpits@thoughtworks.com>
2018-10-03 02:25:29 +0530

    fixing espresso instrumented test

--- app/src/androidTest/java/com/mytaxi/android_demo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/mytaxi/android_demo/ExampleInstrumentedTest.java
@@ -82,7 +82,7 @@ public class ExampleInstrumentedTest {
         onView(withId(R.id.btn_login))
                 .perform(click());
         Thread.sleep(6000);
-        onView(allOf(withId(R.id.textSearch), isDisplayed()))
+        onView(withId(R.id.textSearch))
                 .perform(typeText("sa"), closeSoftKeyboard());
         onView(withText("Sarah Scott"))
                 .inRoot(withDecorView(not(is(mActivity.getWindow().getDecorView()))))
