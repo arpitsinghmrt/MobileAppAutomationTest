5c2e822ec011747e9315e9e7bae2c3f7414ebbc6
Arpit Singh <arpits@thoughtworks.com>
2018-10-04 00:07:10 +0530

    Fixing config.yml

--- .circleci/run-build-locally.sh
+++
@@ -1,6 +0,0 @@
-curl --user ${CIRCLE_TOKEN}: \
-    --request POST \
-    --form revision=25d483d25a1113f6f0e21bbd6f39ff6deefdbda8\
-    --form config=@config.yml \
-    --form notify=false \
-        https://circleci.com/api/v1.1/project/github/arpitsinghmrt/MobileAppAutomationTest/tree/master
